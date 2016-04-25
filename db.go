8779bdbc42c442ae98ddb58fdf39beb1afab1237
Tim Kelsey <tim.kelsey@hp.com>
2016-04-25 10:25:11 +0100

    Running gofmt

--- db.go
+++ db.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "compress/gzip"
-    "database/sql"
-    "encoding/xml"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "time"
-    _ "github.com/mattn/go-sqlite3"
+	"compress/gzip"
+	"database/sql"
+	"encoding/xml"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
 // objects parsed out of the NVD XML stream
 // ---------------------------------------------------------------------------
 
-// Nvd is array of Entry
+// Nvd is an array of Entry
 type Nvd struct {
 	Entries []Entry `xml:"entry"`
 }
@@ -60,26 +60,29 @@ type Link struct {
 // ---------------------------------------------------------------------------
 
 func build_tabs(db *sql.DB) {
-  tx, err := db.Begin(); checkErr(err)
-  sqlStmt := `
+	tx, err := db.Begin()
+	checkErr(err)
+	sqlStmt := `
     create table IF NOT EXISTS Entries
       (CveID TEXT PRIMARY KEY,
        PublishedDate DATETIME,
        LastModifiedDate DATETIME,
        Summary TEXT)`
 
-  _, err = tx.Exec(sqlStmt); checkErr(err)
+	_, err = tx.Exec(sqlStmt)
+	checkErr(err)
 
-  sqlStmt = `
+	sqlStmt = `
     create table IF NOT EXISTS Products
       (Id INTEGER PRIMARY KEY AUTOINCREMENT,
        CveID TEXT,
        Value TEXT,
        FOREIGN KEY(CveID) REFERENCES Entries(CveID))`
 
-  _, err = tx.Exec(sqlStmt); checkErr(err)
+	_, err = tx.Exec(sqlStmt)
+	checkErr(err)
 
-  sqlStmt = `
+	sqlStmt = `
     create table IF NOT EXISTS CveRefs
       (Id INTEGER PRIMARY KEY AUTOINCREMENT,
        CveID TEXT,
@@ -89,9 +92,10 @@ func build_tabs(db *sql.DB) {
        LinkHref TEXT,
        FOREIGN KEY(CveID) REFERENCES Entries(CveID))`
 
-  _, err = tx.Exec(sqlStmt); checkErr(err)
+	_, err = tx.Exec(sqlStmt)
+	checkErr(err)
 
-  sqlStmt = `
+	sqlStmt = `
     create table IF NOT EXISTS Cvss
       (Id INTEGER PRIMARY KEY AUTOINCREMENT,
        CveID TEXT,
@@ -106,108 +110,116 @@ func build_tabs(db *sql.DB) {
        GeneratedOnDate DATETIME,
        FOREIGN KEY(CveID) REFERENCES Entries(CveID))`
 
-  _, err = tx.Exec(sqlStmt); checkErr(err)
-  tx.Commit()
+	_, err = tx.Exec(sqlStmt)
+	checkErr(err)
+	tx.Commit()
 }
 
 func insert_entry(db *sql.DB, entry *Entry) {
-  tx, err := db.Begin(); checkErr(err)
+	tx, err := db.Begin()
+	checkErr(err)
 
-  sqlStmt, err := tx.Prepare(`
+	sqlStmt, err := tx.Prepare(`
     INSERT INTO Entries (CveID, PublishedDate, LastModifiedDate, Summary)
-    VALUES (?, ?, ?, ?)`); checkErr(err)
+    VALUES (?, ?, ?, ?)`)
+	checkErr(err)
 
-  _, err = sqlStmt.Exec(entry.CveID, entry.PublishedDate,
-                        entry.LastModifiedDate, entry.Summary)
-  checkErr(err)
+	_, err = sqlStmt.Exec(entry.CveID, entry.PublishedDate,
+		entry.LastModifiedDate, entry.Summary)
+	checkErr(err)
 
-  sqlStmt, err = tx.Prepare(`
+	sqlStmt, err = tx.Prepare(`
     INSERT INTO Products (CveID, Value)
-    VALUES (?, ?)`); checkErr(err)
-  for _, val := range entry.Products {
-    _, err = sqlStmt.Exec(entry.CveID, val)
-    checkErr(err)
-  }
+    VALUES (?, ?)`)
+	checkErr(err)
+	for _, val := range entry.Products {
+		_, err = sqlStmt.Exec(entry.CveID, val)
+		checkErr(err)
+	}
 
-  sqlStmt, err = tx.Prepare(`
+	sqlStmt, err = tx.Prepare(`
     INSERT INTO CveRefs (CveID, Type, Source, LinkValue, LinkHref)
-    VALUES (?, ?, ?, ?, ?)`); checkErr(err)
-  for _, val := range entry.References {
-    _, err = sqlStmt.Exec(entry.CveID, val.Type, val.Source,
-                          val.Link.Value, val.Link.Href)
-    checkErr(err)
-  }
+    VALUES (?, ?, ?, ?, ?)`)
+	checkErr(err)
+	for _, val := range entry.References {
+		_, err = sqlStmt.Exec(entry.CveID, val.Type, val.Source,
+			val.Link.Value, val.Link.Href)
+		checkErr(err)
+	}
 
-  sqlStmt, err = tx.Prepare(`
+	sqlStmt, err = tx.Prepare(`
     INSERT INTO Cvss (CveID, Score, AccessVector, AccessComplexity,
                       Authentication, ConfidentialityImpact, IntegrityImpact,
                       AvailabilityImpact, Source, GeneratedOnDate)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`); checkErr(err)
-  _, err = sqlStmt.Exec(entry.CveID,
-                        entry.Cvss.Score,
-                        entry.Cvss.AccessVector,
-                        entry.Cvss.AccessComplexity,
-                        entry.Cvss.Authentication,
-                        entry.Cvss.ConfidentialityImpact,
-                        entry.Cvss.IntegrityImpact,
-                        entry.Cvss.AvailabilityImpact,
-                        entry.Cvss.Source,
-                        entry.Cvss.GeneratedOnDate)
-  checkErr(err)
-  tx.Commit()
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+	checkErr(err)
+	_, err = sqlStmt.Exec(entry.CveID,
+		entry.Cvss.Score,
+		entry.Cvss.AccessVector,
+		entry.Cvss.AccessComplexity,
+		entry.Cvss.Authentication,
+		entry.Cvss.ConfidentialityImpact,
+		entry.Cvss.IntegrityImpact,
+		entry.Cvss.AvailabilityImpact,
+		entry.Cvss.Source,
+		entry.Cvss.GeneratedOnDate)
+	checkErr(err)
+	tx.Commit()
 }
 
 func fill_nvd(db *sql.DB) {
-  tpl := "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-%d.xml.gz"
-  for i := 2; i < 16; i++ { // 2002 - 2016
-    url := fmt.Sprintf(tpl, 2000 + i)
-    fmt.Println(url)
-    resp, err := http.Get(url)
-    checkErr(err)
+	tpl := "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-%d.xml.gz"
+	for i := 2; i < 16; i++ { // 2002 - 2016
+		url := fmt.Sprintf(tpl, 2000+i)
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		checkErr(err)
 
-    defer resp.Body.Close()
-    body, err := gzip.NewReader(resp.Body); checkErr(err)
-    data, err := ioutil.ReadAll(body); checkErr(err)
+		defer resp.Body.Close()
+		body, err := gzip.NewReader(resp.Body)
+		checkErr(err)
+		data, err := ioutil.ReadAll(body)
+		checkErr(err)
 
-    var q Nvd
-	  err = xml.Unmarshal(data, &q)
-    fmt.Println(len(q.Entries))
-    checkErr(err)
+		var q Nvd
+		err = xml.Unmarshal(data, &q)
+		fmt.Println(len(q.Entries))
+		checkErr(err)
 
-    for _, val := range q.Entries {
-      insert_entry(db, &val)
-    }
-  }
+		for _, val := range q.Entries {
+			insert_entry(db, &val)
+		}
+	}
 }
 
 func update_nvd(db *sql.DB) {
-  //TODO: just update new data
+	//TODO: just update new data
 }
 
 func main() {
-  // path should probably come form stdin ...
-  path := "./nvd.db"
-  full := false
+	// path should probably come form stdin ...
+	path := "./nvd.db"
+	full := false
 
-  // do a full update if the DB file is missing
-  if _, err := os.Stat(path); os.IsNotExist(err) {
-    full = true
-  }
+	// do a full update if the DB file is missing
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		full = true
+	}
 
-  db, err := sql.Open("sqlite3", path)
-  checkErr(err)
+	db, err := sql.Open("sqlite3", path)
+	checkErr(err)
 
-  if full {
-    build_tabs(db)
-    fill_nvd(db)
-  } else {
-    update_nvd(db)
-  }
-  db.Close()
+	if full {
+		build_tabs(db)
+		fill_nvd(db)
+	} else {
+		update_nvd(db)
+	}
+	db.Close()
 }
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
